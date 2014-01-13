c7bc0e3259b350faf5f6105b4ad7a092a48ddc09
Ryan Gocal <rgocal09@gmail.com>
2014-01-13 17:00:05 -0500

    360 Launcher Themer 1.0 Alpha
    -WIP- Under Construction, Backing up current code in Github. Application
    is not fully operatable.

---
+++ gen/com/NxIndustries/theme/Sense/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.NxIndustries.theme.Sense;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/NxIndustries/theme/Sense/MainActivity.java
@@ -0,0 +1,94 @@
+package com.NxIndustries.theme.Sense;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.app.Activity;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	
+	final static String TARGET_BASE_PATH = "/sdcard/360launcher/theme/";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	
+	final Button iButton = (Button) findViewById(R.id.install);
+    iButton.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+        	copyFileOrDir("");
+    }
+    });
+	}
+
+		private void copyFileOrDir(String path) {
+			AssetManager assetManager = this.getAssets();
+		    String assets[] = null;
+		    try {
+		        Log.i("tag", "copyFileOrDir() "+path);
+		        assets = assetManager.list(path);
+		        if (assets.length == 0) {
+		            copyFile(path);
+		        } else {
+		            String fullPath =  TARGET_BASE_PATH + path;
+		            Log.i("tag", "path="+fullPath);
+		            File dir = new File(fullPath);
+		            if (!dir.exists() && !path.startsWith("images") && !path.startsWith("sounds") && !path.startsWith("webkit"))
+		                if (!dir.mkdirs());
+		                    Log.i("tag", "could not create dir "+fullPath);
+		            for (int i = 0; i < assets.length; ++i) {
+		                String p;
+		                if (path.equals(""))
+		                    p = "";
+		                else 
+		                    p = path + "/";
+
+		                if (!path.startsWith("images") && !path.startsWith("sounds") && !path.startsWith("webkit"))
+		                    copyFileOrDir( p + assets[i]);
+		            }
+		        }
+		    } catch (IOException ex) {
+		        Log.e("tag", "I/O Exception", ex);
+		    }
+		}
+
+		private void copyFile(String filename) {
+			AssetManager assetManager = this.getAssets();
+
+		    InputStream in = null;
+		    OutputStream out = null;
+		    String newFileName = null;
+		    try {
+		        Log.i("tag", "copyFile() "+filename);
+		        in = assetManager.open(filename);
+		        if (filename.endsWith(".jpg")) // extension was added to avoid compression on APK file
+		            newFileName = TARGET_BASE_PATH + filename.substring(0, filename.length()-4);
+		        else
+		            newFileName = TARGET_BASE_PATH + filename;
+		        out = new FileOutputStream(newFileName);
+
+		        byte[] buffer = new byte[1024];
+		        int read;
+		        while ((read = in.read(buffer)) != -1) {
+		            out.write(buffer, 0, read);
+		        }
+		        in.close();
+		        in = null;
+		        out.flush();
+		        out.close();
+		        out = null;
+		    } catch (Exception e) {
+		        Log.e("tag", "Exception in copyFile() of "+newFileName);
+		        Log.e("tag", "Exception in copyFile() "+e.toString());
+		    }
+		}
+}
